b0235a37fffec063ec3fb6acdb40f2f873cf7680
rollan <rollantheminion@gmail.com>
2024-10-08 19:41:53 -0700

    deployment commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,7 +7,7 @@ const { test, registerUser, loginUser, getProfile } = require('../controllers/au
 router.use(
     cors({
         credentials: true,
-        origin: 'http://localhost:5173'
+        origin: 'https://cssa-membership.onrender.com'
     })
 )
 
